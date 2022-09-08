1b35cfd66ba4e20574168ceb634ff24de2359339
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-08 09:31:01 +0700

    feat : fetch quotes data using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
 
   const [quotes, setQuotes] = useState([]);
   const [isLoading, setLoading] = useState(true);
-  
-  useEffect(()=>{
-  
-  });
+
+  useEffect(() => {
+
+    const fetchQuote = async () => {
+      const result = await axios(`https://type.fit/api/quotes`) 
+      // console.log(result.data);
+      setQuotes(result.data);
+      setLoading(false);
+      
+    }
+    fetchQuote();
+  },[]);
 
   return (
     <div className="App">
