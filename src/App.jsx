92a5c446f3853b19fd13ec936978f7826ad7fa06
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-08 16:54:45 +0700

    fix: remove blockquote , p, button and anchor

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import DisplayQuote from './components/DisplayQuote';
 import './App.css';
 
-function App() {
+const App = () => {
 
   const [quotes, setQuotes] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-
     const fetchQuote = async () => {
       const result = await axios(`https://type.fit/api/quotes`) 
       // console.log(result.data);
       setQuotes(result.data);
       setLoading(false);
-      
     }
     fetchQuote();
-  },[]);
+    },[]);
 
   return (
     <div className="App">
       <main id="quote-box">
-     
-        <blockquote id="text">
-
-          <p>{/* {quote text variable here} */}</p>
-
-          <cite id="author"> 
-        {/* author name variable here */}
-          </cite>
-
-        </blockquote>
-        
-        <button id="new-quote"> </button>
-        <a href="#" id="tweet-quote"> </a>
-
+        <DisplayQuote quotes={quotes} isLoading={isLoading} />
       </main>
     </div>
   )
