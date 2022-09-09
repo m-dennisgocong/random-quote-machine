a694ed07c36b9ff0c62b97763626d8585bfd617e
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-09 20:17:03 +0700

    style: change the icon title

--- src/components/DisplayQuote.jsx
+++ src/components/DisplayQuote.jsx
@@ -23,7 +23,7 @@ const DisplayQuote = ({quotes, isLoading}) => {
           <cite id="author">- {quote.author}</cite>
         </blockquote>
         <div className="buttonContainer">
-          <a id="tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&text='+encodeURIComponent('"'+quote.text+'" - '+quote.author)} target="_top" className="shareTweet"><i class="fa fa-twitter" title="Share Tweet"></i></a>
+          <a id="tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&text='+encodeURIComponent('"'+quote.text+'" - '+quote.author)} target="_top" className="shareTweet"><i class="fa fa-twitter" title="Share to Twitter!"></i></a>
           <a onClick={nextRandomQuote} id="new-quote"><i class="fa fa-random" aria-hidden="true" title="New Random Quote"></i></a>
         </div>
       </>
