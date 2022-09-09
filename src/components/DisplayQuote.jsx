a45f1778cc1bcb1fe69c0553dc64105fa1b0fe53
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-09 16:20:58 +0700

    feat: import css and remove shareTweet function
    in order to pass the FCC-test i need put the 'a tag' together with href

--- src/components/DisplayQuote.jsx
+++ src/components/DisplayQuote.jsx
@@ -1,9 +1,9 @@
 import React,{useEffect, useState} from "react";
-import TwitterLogo from './../assets/twitterLogo.svg';
+import './DisplayQuote.css'
+
 const DisplayQuote = ({quotes, isLoading}) => {
 
     const randomQuote = () => quotes[(Math.floor(Math.random() * quotes.length) + 1)];
-
     const [quote,setQuote] = useState({quote : "", author : ""});
 
     useEffect(() => {
@@ -16,19 +16,16 @@ const DisplayQuote = ({quotes, isLoading}) => {
       setQuote({...quote, ...getRandomQuote});
     }
 
-    const shareTweet = () => {
-      document.getElementById('tweet-quote').setAttribute('href','https://twitter.com/intent/tweet?hashtags=quotes&text='+encodeURIComponent('"'+quote.text+'" - '+quote.author))
-    } 
-
     return isLoading ? <h1> Loading... </h1> : 
       <>
         <blockquote id="text">
-          <p>{quote.text}</p>
-          <cite id="author">{quote.author}</cite>
+          <p><q>{quote.text}</q> </p>
+          <cite id="author">- {quote.author}</cite>
         </blockquote>
-        <button onClick={nextRandomQuote} id="new-quote"> Next </button>
-        <a onClick={shareTweet} id="tweet-quote" target="_top" title="Tweet this quote!" className="share"> <img src={TwitterLogo} alt="Twitter Logo" /></a>
-        
+        <div className="buttonContainer">
+          <a id="tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&text='+encodeURIComponent('"'+quote.text+'" - '+quote.author)} target="_top" className="shareTweet"><i class="fa fa-twitter" title="Share Tweet"></i></a>
+          <a onClick={nextRandomQuote} id="new-quote"><i class="fa fa-random" aria-hidden="true" title="New Random Quote"></i></a>
+        </div>
       </>
 }
 export default DisplayQuote;
