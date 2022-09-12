683d7e250e5ca0be7455a4508df47cfebf8cbe53
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-12 14:19:46 +0700

    perf: remove unnecessary state
    since  props randomQuote already have the needed data

--- src/components/DisplayQuote.jsx
+++ src/components/DisplayQuote.jsx
@@ -1,24 +1,19 @@
-import React,{useEffect, useState} from "react";
 import './DisplayQuote.css';
 
-const DisplayQuote = ({randomQuote, isLoading, fetchQuote}) => {
-
-  const [quote,setQuote] = useState({content : "", author : ""});
-
-  useEffect(() => {
-    setQuote({...quote, ...randomQuote});      
-  },[randomQuote]);
-
-  return isLoading ? <h1> Loading... </h1> : 
+const DisplayQuote = ({randomQuote,fetchQuote}) => {
+  const {content, author} = randomQuote;
+  return( 
   <>
     <blockquote id="text">
-      <p><q>{quote.content}</q> </p>
-      <cite id="author">- {quote.author}</cite>
+      <p><q>{content}</q> </p>
+      <cite id="author">- {author}</cite>
     </blockquote>
+
     <div className="buttonContainer">
-      <a id="tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&text='+encodeURIComponent('"'+quote.content+'" - '+quote.author)} target="_top" className="shareTweet"><i className="fa fa-twitter" title="Share to Twitter!"></i></a>
+      <a id="tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&text='+encodeURIComponent('"'+content+'" - '+author)} target="_top" className="shareTweet"><i className="fa fa-twitter" title="Share to Twitter!"></i></a>
       <a onClick={fetchQuote} id="new-quote"><i className="fa fa-random" aria-hidden="true" title="New Random Quote"></i></a>
     </div>
   </>
+  );
 }
 export default DisplayQuote;
